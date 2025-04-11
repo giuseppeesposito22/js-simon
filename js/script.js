7c36f0d3a3f64c2c25fae55fb76d4d0b54c183bc
giuseppeesposito22 <gius.espo22@gmail.com>
2025-04-11 15:44:49 +0200

    creato timer e aggiunto nel dom

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
+const countdownEl = document.getElementById("countdown");
 
+// con il setInterval creo un countdown
+let tempo = 30000;
 
+const interval = setInterval(() => {
+  tempo -= 1000;
+  countdownEl.innerHTML = tempo / 1000;
+
+  if (tempo === 0) clearInterval(interval);
+}, 1000);
